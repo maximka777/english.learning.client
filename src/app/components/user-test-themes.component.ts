2bd83bdfb13d048c1208e76e26c2f41d5420a4a9
lebedeanastasija <lebedeanastasija@yandex.by>
2017-05-20 14:12:47 +0300

    Fix modal size and accessibility

--- src/app/components/user-test-themes.component.ts
+++ src/app/components/user-test-themes.component.ts
@@ -1,10 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {WordThemesService} from "../services/word-themes.service";
 import {WordTheme} from "../models/WordTheme";
+import {TestTheme} from "../models/TestTheme";
 import {WordsService} from "../services/words.service";
 import {TestThemesService} from "../services/test-themes.service";
 import {TestsService} from "../services/tests.service";
 import {AuthService} from "../services/auth.service";
+import {AlertService} from "../services/alert.service";
+import {ModalDirective} from "ng2-bootstrap";
 
 @Component({
   selector: 'user-test-themes',
@@ -16,10 +19,16 @@ export class UserTestThemesComponent implements OnInit {
   tests = null;
   selectedThemeId: number;
   selectedTestId: number;
+  currentTheory: String;
+  theme: TestTheme;
+
+  @ViewChild('theoryModal') public theoryModal: ModalDirective;
 
   constructor(private testThemesService: TestThemesService,
               private authService: AuthService,
-              private testsService: TestsService) {
+              private testsService: TestsService,
+              private alertService: AlertService
+  ) {
     testThemesService.getAll()
       .then((themes: [any]) => {
         this.themes = themes;
@@ -35,11 +44,35 @@ export class UserTestThemesComponent implements OnInit {
     }
   }
 
+  openTheoryModalForView() {
+    if(!this.selectedThemeId) {
+      return this.alertService.showErrorMessage('Не одна тема не выбрана') ;
+    }
+    if(!this.currentTheory || !this.currentTheory.length) {
+      return this.alertService.showErrorMessage('Отсутствует теория по данной теме') ;
+    }
+    this.openTheoryModal();
+  }
+
+  openTheoryModal() {
+    this.theoryModal.show();
+  }
+
+  closeTheoryModal() {
+    this.theoryModal.hide();
+  }
+
+  onTheoryFormClose() {
+    this.closeTheoryModal();
+  }
+
   selectTestTheme(themeId) {
     this.selectedThemeId = themeId;
     this.testsService.getAll(themeId)
       .then(tests => {
         this.tests = tests;
+        this.theme = this.themes.filter(t => t.id === themeId)[0];
+        this.currentTheory = this.theme.theory;
       });
   }
 
